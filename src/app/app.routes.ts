086c3d1450a95ce8445667965fcaf988e065f02f
Jared Ricioli <jared.ricioli@live.com>
2024-08-15 16:15:31 -0300

    fix: rota principal também leva para a tela de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-{
-  path: 'login',
-  component: LoginComponent
-}
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '',
+    component: LoginComponent
+  }
 ];
