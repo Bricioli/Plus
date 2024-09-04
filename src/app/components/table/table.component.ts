5f050a0438a39d5b42627bd566b3ab23d1a9c927
Jared Ricioli <jared.ricioli@live.com>
2024-09-04 18:57:41 -0300

    feat/adicionada a table paginação e filtro

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,8 +1,8 @@
 // import { UserData } from './table.component';
-import { Component, OnInit } from '@angular/core';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import { MatTableModule } from '@angular/material/table';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
@@ -30,15 +30,34 @@ export interface NurseData {
   standalone: true,
   imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule, MatCardModule, MatToolbarModule],
 })
-export class TableList implements OnInit {
+export class TableList implements OnInit, AfterViewInit {
   displayedColumns: string[] = ['name', 'worked', 'receive'];
-  dataSource: NurseData[] = [];
+  dataSource: MatTableDataSource<NurseData>
 
-  constructor(private nurseService: NursesService) {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private nurseService : NursesService) {
+    this.dataSource = new MatTableDataSource();
   }
+
   ngOnInit(): void {
     this.nurseService.getNurses().subscribe((nurses) => {
-      this.dataSource = nurses;
+      this.dataSource = new MatTableDataSource(nurses);
     })
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
